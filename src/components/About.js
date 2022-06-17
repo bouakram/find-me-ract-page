1395a1c1a12279bc6773b255229da9ffdc53824b
Akrem Boughazi <kikoua203@gmail.com>
2022-06-17 19:56:29 +0100

    just create my small info page

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import MyPic from '../images/me.png'
+
+function About() {
+  return (
+    <div className='About'>
+        <img src={MyPic} alt="akram pictur" />
+
+        <h1>Boughazi Akram</h1>
+
+        <p>Computer Scienc (student) , self taught FrontEnd Developer (React Js 💕) , I live in Algeria 🇩🇿 </p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/components/Links.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Links() {
+  return (
+    <div className='Links'>
+        <ul>
+            <li><a href="https://twitter.com/akrem_QLF" target="_blank"> twitter  <i class="ri-twitter-fill"></i></a></li>
+            <li><a href="https://github.com/bouakram" target="_blank" rel="noopener noreferrer"> github <i class="ri-github-fill"></i></a></li>
+            <li><a href="https://www.linkedin.com/in/akram-boughazi-b3956a212/" target="_blank" rel="noopener noreferrer"> linkedin <i class="ri-linkedin-fill"></i></a></li>
+            <li><a href="#"> portfolio <small>(comming soon !)</small> <i class="ri-global-fill"></i></a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Links
