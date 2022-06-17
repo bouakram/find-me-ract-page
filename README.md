71bd6ae9da6e371cf216a1db70ad6ceb6533503b
Akrem Boughazi <kikoua203@gmail.com>
2022-06-17 20:13:18 +0100

    just adding a footer and some edit

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# Getting Started with Create React App
+# just create a web page about me and some links 
 
+# Getting Started with Create React App
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import About from './components/About';
 import Links from './components/Links';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <About />
       <Links />
+      <Footer />
     </div>
   );
 }
